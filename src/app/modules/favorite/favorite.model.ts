a961d69d7595c964e669ac731123c70051a220f4
emonhassan83 <emonhasan7650@gmail.com>
2026-02-25 11:59:14 +0600

    some service module modified

--- src/app/modules/favorite/favorite.model.ts
+++ src/app/modules/favorite/favorite.model.ts
@@ -4,7 +4,7 @@ import { TFavorite, TFavoriteModel } from './favorite.interface';
 const favoriteSchema = new Schema<TFavorite>(
   {
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    service: { type: Schema.Types.ObjectId, ref: 'Deal', required: true },
+    service: { type: Schema.Types.ObjectId, ref: 'Service', required: true },
   },
   {
     timestamps: true
